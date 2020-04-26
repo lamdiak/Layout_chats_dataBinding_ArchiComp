00a28f7600d80f8135e9490f871e6b44785466b2
lamdiak <lamine_diakit@yahoo.fr>
2020-04-26 13:07:50 +0200

    Save Note
    Save Note after modify

--- app/src/main/java/note/NoteDetailActivity.kt
+++ app/src/main/java/note/NoteDetailActivity.kt
@@ -1,14 +1,20 @@
 package note
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
 import archcomps.traning.R
 
 
 class NoteDetailActivity : AppCompatActivity() {
 
     companion object {
+        val REQUEST_EDIT_NOTE = 1
         val EXTRA_NOTE = "note"
         val EXTRA_NOTE_INDEX = "noteIndex"
     }
@@ -22,13 +28,44 @@ class NoteDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_detail)
 
+        // Toolbar
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
         note = intent.getParcelableExtra(EXTRA_NOTE)
         noteIndex = intent.getIntExtra(EXTRA_NOTE_INDEX, -1)
         textTitle = findViewById(R.id.editText5)
         textBody = findViewById(R.id.editText6)
         textTitle.text = note.title
-        textBody.text = note?.body
+        textBody.text = note.body
 
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.option_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.save -> {
+                saveNoteDetail()
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+
+    }
+
+    private fun saveNoteDetail() {
+        note.title = textTitle.text.toString()
+        note.body = textBody.text.toString()
+        intent = Intent()
+        intent.putExtra(EXTRA_NOTE, note)
+        intent.putExtra(EXTRA_NOTE_INDEX, noteIndex)
+        setResult(Activity.RESULT_OK, intent)
+        finish()
     }
 }
