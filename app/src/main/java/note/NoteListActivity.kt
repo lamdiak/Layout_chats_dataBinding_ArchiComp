0d5037102fc8531813bfb8b4b0664b893951c2c6
lamdiak <lamine_diakit@yahoo.fr>
2020-04-26 13:07:50 +0200

    Save Note
    Save Note after modify

--- app/src/main/java/note/NoteListActivity.kt
+++ app/src/main/java/note/NoteListActivity.kt
@@ -1,13 +1,18 @@
 package note
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import archcomps.traning.R
+import note.NoteDetailActivity.Companion.EXTRA_NOTE
+import note.NoteDetailActivity.Companion.EXTRA_NOTE_INDEX
+import note.NoteDetailActivity.Companion.REQUEST_EDIT_NOTE
 
 class NoteListActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -19,6 +24,10 @@ class NoteListActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_list)
+
+        // Toolbar
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
         // My List
         note = mutableListOf()
@@ -33,8 +42,23 @@ class NoteListActivity : AppCompatActivity(), View.OnClickListener {
             layoutManager = viewManager
             adapter = myAdapterNote
         }
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode != REQUEST_EDIT_NOTE && data==null){
+           return
+       }
+        when(requestCode){
+            REQUEST_EDIT_NOTE -> processEditNoteResult(data)
+        }
 
+    }
+
+    private fun processEditNoteResult(data: Intent?) {
+        val noteIndex = intent.getIntExtra(EXTRA_NOTE_INDEX, -1)
+        val notes = intent.getParcelableExtra<Note>(EXTRA_NOTE)
+        saveNote(notes,noteIndex)
     }
 
     override fun onClick(v: View) {
@@ -44,11 +68,18 @@ class NoteListActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
+
+    private fun saveNote(notes: Note, noteIndex: Int) {
+        note[noteIndex] = notes
+        myAdapterNote.notifyDataSetChanged()
+    }
+
     private fun showNoteDetail(noteIndex: Int) {
         var notes = note[noteIndex]
         val intent = Intent(this, NoteDetailActivity::class.java)
-        intent.putExtra(NoteDetailActivity.EXTRA_NOTE,notes)
-        intent.putExtra(NoteDetailActivity.EXTRA_NOTE_INDEX,noteIndex)
-        startActivity(intent)
+        intent.putExtra(EXTRA_NOTE,notes)
+        intent.putExtra(EXTRA_NOTE_INDEX,noteIndex)
+        startActivityForResult(intent, REQUEST_EDIT_NOTE)
     }
+
 }
